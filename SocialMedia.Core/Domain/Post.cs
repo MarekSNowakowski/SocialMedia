9078bbe423ad523fdd01db02cecf14c5c0649e41
Marek Nowakowski <marsznow@gmail.com>
2021-12-20 18:28:38 +0100

    Dodaj pliki projektów.

---
+++ SocialMedia.Core/Domain/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SocialMedia.Core.Domain
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime Time { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
---
+++ SocialMedia.Core/Repositories/IPostRepository.cs
@@ -0,0 +1,15 @@
+﻿using SocialMedia.Core.Domain;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Core.Repositories
+{
+    public interface IPostRepository
+    {
+        public Task<IEnumerable<Post>> BrowseAllAsync();
+        public Task<Post> GetAsync(int id);
+        public Task AddAsync(Post s);
+        public Task DelAsync(int id);
+        public Task UpdateAsync(Post s);
+    }
+}
