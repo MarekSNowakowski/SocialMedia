82b805cfde5dfb9775479959a2f9780c38f42972
Marek Nowakowski <marsznow@gmail.com>
2021-12-23 21:43:52 +0100

    Account controller, views, login model and _Layout changes

---
+++ SocialMedia.WebApp/Controllers/AccountController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Zawodnicy.WebApp.Models;
+
+namespace Zawodnicy.WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        //Loging POST
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginVM);
+            }
+
+            // returns user name to login
+            var user = await _userManager.FindByNameAsync(loginVM.UserName);
+
+            if(user != null)
+            {
+                // login try
+                var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ModelState.AddModelError("", "Niepoprawna nazwa użytkownika lub hasło...");
+
+            return View(loginVM);
+        }
+
+        public IActionResult Register()
+        {
+            return View(new LoginVM());
+        }
+
+        //Registering POST
+        [HttpPost]
+        public async Task<IActionResult> Register(LoginVM loginVM)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new IdentityUser() { UserName = loginVM.UserName };
+                var result = await _userManager.CreateAsync(user, loginVM.Password);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");   //(metoda, controller)
+                }
+            }
+
+            return View(loginVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
